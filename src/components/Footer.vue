af23351130127cedbdf263c5c0b947f4699789aa
Henrik Jangefelt <henke9105@hotmail.com>
2020-01-16 22:02:38 +0100

    Firebase added to project

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container-fluid pt-5">
+      <div class="row d-flex align-items-center">
+        <div class="col-md-12 col-lg-12">
+          <p class="text-center text-white pt-5 mt-5">© 2020 MOVIES</p>
+        </div>
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container-fluid{
+  height: 200px;
+  background: #2672ffb6;
+  background: linear-gradient(to bottom, #24243e7a, #302b63, #15026bdc); 
+}
+
+</style>
