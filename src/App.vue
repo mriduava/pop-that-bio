6a92773b0daa568b913207c28ef1574909e0c153
mriduava <mriduava@gmail.com>
2020-01-17 17:11:46 +0100

    fix bug & delete unused comp & files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/home">Pop-That-Bio</router-link> |
+      <router-link to="/home">Pop that Bio</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
