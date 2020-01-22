2b314f77956e8e2f980f030a58a42bdaaa42aa01
Ola Wiebe <eeken07@gmail.com>
2020-01-22 10:49:25 +0100

    fix error

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import 'materialize-css'
 import 'materialize-css/dist/css/materialize.css'
-import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
