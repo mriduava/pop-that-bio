97f04b3189ffbc8d9fafff53230090b8ca294ef3
Ola Wiebe <eeken07@gmail.com>
2020-01-24 09:51:06 +0100

    edit footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,115 +1,128 @@
 <template>
   <div class="container-fluid pt-5">
-      <div class="row d-flex align-items-center">
-        <hr>
-        <div class="col-md-12 col-lg-12">
-        
-     
-          <div class="Gäster-list">
-            <div class="grop">
-              <h5>Gäster</h5>
-              <a href="https://www.w3schools.com/tags/default.asp"><p>Kundservice</p></a>
-               <a href="https://www.w3schools.com/tags/default.asp"><p>Frågor och svar</p></a>
-                <a href="https://www.w3schools.com/tags/default.asp"><p>Boka större grupper</p></a>
-                 <a href="https://www.w3schools.com/tags/default.asp"><p>Mina reservationer</p></a>
-                  <a href="https://www.w3schools.com/tags/default.asp"><p>Lämna feedback</p></a>
-            </div>
-            <div class="grop">
-               <h5>Medlemmar</h5>
-                <a href="https://www.w3schools.com/tags/default.asp"><p>Kundservice</p></a>
-               <a href="https://www.w3schools.com/tags/default.asp"><p>Frågor och svar</p></a>
-                <a href="https://www.w3schools.com/tags/default.asp"><p>Boka större grupper</p></a>
-                 <a href="https://www.w3schools.com/tags/default.asp"><p>Mina reservationer</p></a>
-                  <a href="https://www.w3schools.com/tags/default.asp"><p>Lämna feedback</p></a>
-            </div>
-            <div class="grop">
-               <h5>Företag</h5>
-                <a href="https://www.w3schools.com/tags/default.asp"><p>Kundservice</p></a>
-               <a href="https://www.w3schools.com/tags/default.asp"><p>Frågor och svar</p></a>
-                <a href="https://www.w3schools.com/tags/default.asp"><p>Boka större grupper</p></a>
-                 <a href="https://www.w3schools.com/tags/default.asp"><p>Mina reservationer</p></a>
-                  <a href="https://www.w3schools.com/tags/default.asp"><p>Lämna feedback</p></a>
-            </div>
-             <div class="grop">
-               <h5>Företag</h5>
-               
-                <a href="https://www.w3schools.com/tags/default.asp"><p>Kundservice</p></a>
-               <a href="https://www.w3schools.com/tags/default.asp"><p>Frågor och svar</p></a>
-                <a href="https://www.w3schools.com/tags/default.asp"><p>Boka större grupper</p></a>
-                 <a href="https://www.w3schools.com/tags/default.asp"><p>Mina reservationer</p></a>
-                  <a href="https://www.w3schools.com/tags/default.asp"><p>Lämna feedback</p></a>
-            </div>
-             
-           </div>
-                
-               </div>
-              <div>
-                 <a href="http://facebook.com/"><i class="fab fa-facebook"></i></a>
-                 <a href="http://twitter.com/"><i class="fab fa-twitter"></i></a>
-                 <a href="http://youtube.com/"><i class="fab fa-youtube"></i></a>
-                 <p class="text-center text-white pt-5 mt-5">POP-THIS-BIO © 2020 MOVIES</p> 
-             </div>
-      </div>
+    <div class="row d-flex align-items-center">
+      <hr />
+      <div class="col-md-12 col-lg-12">
+        <div class="Gäster-list">
+          <div class="group1">
+            <h5>Gäster</h5>
+            <a href="https://www.w3schools.com/tags/default.asp">
+              <p>Kundservice</p>
+            </a>
+            <a href="https://www.w3schools.com/tags/default.asp">
+              <p>Frågor och svar</p>
+            </a>
+          </div>
+          <div class="group2">
+            <h5>Medlemmar</h5>
+            <a href="https://www.w3schools.com/tags/default.asp">
+              <p>Kundservice</p>
+            </a>
+            <a href="https://www.w3schools.com/tags/default.asp">
+              <p>Frågor och svar</p>
+            </a>
+          </div>
 
+          <div class="group3">
+            <h5>Företag</h5>
+            <a href="https://www.w3schools.com/tags/default.asp">
+              <p>Kundservice</p>
+            </a>
+            <a href="https://www.w3schools.com/tags/default.asp">
+              <p>Frågor och svar</p>
+            </a>
+          </div>
+          <div class="group4">
+            <h5>Företag</h5>
+
+            <a href="https://www.w3schools.com/tags/default.asp">
+              <p>Kundservice</p>
+            </a>
+            <a href="https://www.w3schools.com/tags/default.asp">
+              <p>Frågor och svar</p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div>
+        <a href="http://facebook.com/">
+          <i class="fab fa-facebook"></i>
+        </a>
+        <a href="http://twitter.com/">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="http://youtube.com/">
+          <i class="fab fa-youtube"></i>
+        </a>
+        <p class="text-center text-white pt-5 mt-5">POP-THIS-BIO © 2020 MOVIES</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'footer',
+  name: "footer",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <style scoped lang="css">
-.container-fluid{
+.container-fluid {
   color: rgb(81, 67, 94);
   padding: 1% 5%;
   /* height: 500px; */
   background: rgb(100, 10, 60);
-  background: -webkit-linear-gradient(to top, rgb(156, 36, 100), #FBD3E9); 
+  background: -webkit-linear-gradient(to top, rgb(156, 36, 100), #fbd3e9);
   background: linear-gradient(to top, rgb(117, 9, 67), rgb(219, 166, 195));
 }
 
-a{
+a {
   color: rgb(233, 233, 233);
 }
 
-.fab{
+.fab {
   font-size: 24px;
   color: #fff;
-  padding:0.5%;
+  padding: 0.5%;
   margin: 30px auto;
- 
 }
-.Gäster-list{
+.Gäster-list {
   /* max-width: 1300px; */
   margin: 40px auto 0 auto;
-  padding:20px 50px;
-  justify-content:space-between;
+  padding: 20px 50px;
+  justify-content: space-around;
   display: flex;
-  
-}
-@media (min-width: 700px) {
-  .Gäster-list{
-  justify-content:first baseline;
-  display: flex;
-}
-}
-.grop1{
- padding :20px;
- box-sizing: border-box;
- margin-bottom: 20px;
- flex-basis: 30%;
-  
-}
-.text-center text-white pt-5 mt-5{
-   font-size: 24px;
-    color: #fff;
-    float: left;
-   margin: 100px auto
+  flex-direction: column;
 }
 
+/*@media (min-width: 768px) {
+  .Gäster-list {
+    justify-content: first baseline;
+    display: flex;
+    flex-direction: row;
+  }
+}*/
+
+@media (min-width: 700px) {
+  .Gäster-list {
+    justify-content: first baseline;
+    display: flex;
+    flex-direction: row;
+  }
+}
+.group {
+  padding: 20px;
+  box-sizing: border-box;
+  margin-bottom: 20px;
+  flex-basis: 30%;
+}
+.text-center text-white pt-5 mt-5 {
+  font-size: 24px;
+  color: #fff;
+  float: left;
+  margin: 100px auto;
+}
 </style>
