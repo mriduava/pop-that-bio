fa6604147d204b38553a59d67336702862c5a111
mriduava <mriduava@gmail.com>
2020-01-26 21:42:24 +0100

    add font & active menu css to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -22,17 +22,66 @@
 </template>
 
 <style lang="css" scoped>
+@font-face {
+  font-family: borntogrille;
+  src: url("../assets/fonts/borntogrille.otf");
+}
+
 nav{
-  background: rgba(197, 49, 99, 0.5);
+  padding: 0 14.8%;
   border-bottom: 1px solid rgb(213,187,47);
+  background: rgb(100, 10, 60);
+  background: -webkit-linear-gradient(to top, rgb(156, 36, 100), rgba(197, 49, 99, 0.5));
+  background: linear-gradient(to bottom, rgb(117, 9, 67), rgba(197, 49, 99, 0.5));
 }
 
 .brand-logo{
-  padding-left: 2%;
+  font-size: 2.5rem;
+  padding-left: 1.5%;
+  font-family: borntogrille;
+  text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.3);
 }
 
-#nav-mobile{
-  padding-right: 2%;
+.brand-logo:hover{
+  color: rgb(243, 144, 197);
+}
+
+#nav-mobile .nav-item .nav-link{
+  font-size: 1.2em;
+}
+.nav-link:hover{
+  color: rgb(243, 144, 197);
+}
+
+.router-link-active{
+  background: rgb(150, 38, 97);
+}
+
+
+/* RESPONSIVE */
+@media (min-width: 1281px) {  
+}
+
+@media (min-width: 1025px) and (max-width: 1280px) {    
+}
+
+@media (min-width: 768px) and (max-width: 1024px) {
+}
+
+@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+
+}
+
+@media (min-width: 481px) and (max-width: 767px) {
+  nav{
+    padding: 0;
+  }
+}
+
+@media (min-width: 320px) and (max-width: 480px) {
+  nav{
+    padding: 0;
+  }
 }
   
 </style>
