44be6394c1aa59bbb117f5847724c1d51b3cfce8
Hampus Nilsson <hampus10699@gmail.com>
2020-01-27 08:31:31 +0100

    Action to send data to firebase and test method in singin.vue

--- src/store/index.js
+++ src/store/index.js
@@ -28,6 +28,17 @@ export default new Vuex.Store({
         movies.push(e.data())        
       });
       commit('UPDATE_DATA', movies)
+    },
+    async sendToFirebase(context, purchase){
+      let collection = purchase.collection
+      delete purchase.collection
+      await db.collection(collection).add(purchase)
+      /*
+        To work with sendToFirebase
+        when dispatch to this action there need to be a collection property. 
+        Collection property selects the collection in firebase
+        and then deletes it from the object it saves to firebase.
+      */
     }
   },
   modules: {
