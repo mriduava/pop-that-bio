7145d1563526aada1ccae09949f8d4d2b12161ac
Ola Wiebe <eeken07@gmail.com>
2020-01-27 12:06:51 +0100

    edit carousel

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="carousel">
-    <a class="carousel-item" href="#one!"><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi1.ytimg.com%2Fvi%2FPB5FosTwM8s%2Fmaxresdefault.jpg"></a>
-    <a class="carousel-item" href="#two!"><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi1.ytimg.com%2Fvi%2FPB5FosTwM8s%2Fmaxresdefault.jpg"></a>
+  <div class="carousel carousel-slider">
+    <a v-for="i in 5" :key="i" class="carousel-item" href="#one!"><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi1.ytimg.com%2Fvi%2FPB5FosTwM8s%2Fmaxresdefault.jpg"></a>
+    <!-- <a class="carousel-item" href="#two!"><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi1.ytimg.com%2Fvi%2FPB5FosTwM8s%2Fmaxresdefault.jpg"></a>
     <a class="carousel-item" href="#three!"><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi1.ytimg.com%2Fvi%2FPB5FosTwM8s%2Fmaxresdefault.jpg"></a>
     <a class="carousel-item" href="#four!"><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi1.ytimg.com%2Fvi%2FPB5FosTwM8s%2Fmaxresdefault.jpg"></a>
-    <a class="carousel-item" href="#five!"><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi1.ytimg.com%2Fvi%2FPB5FosTwM8s%2Fmaxresdefault.jpg"></a>
+    <a class="carousel-item" href="#five!"><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi1.ytimg.com%2Fvi%2FPB5FosTwM8s%2Fmaxresdefault.jpg"></a> -->
   </div>
 </template>
 
@@ -12,7 +12,10 @@
 export default {
 mounted() {
     var elems = document.querySelectorAll('.carousel');
-    this.$M.Carousel.init(elems, {});
+    this.$M.Carousel.init(elems, {
+      fullWidth: true,
+      autoplay: true
+    });
 }
   
 }
