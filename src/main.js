b24e846b689528e3f747664c1b5eeffc4fd24a93
Ola Wiebe <eeken07@gmail.com>
2020-01-27 10:08:13 +0100

    making scroll to top after link

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'materialize-css'
+import M from 'materialize-css'
 import 'materialize-css/dist/css/materialize.css'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$M = M
+Vue.$M = Vue.prototype.$M
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,9 @@ export default {
   components: {
     Carousel,
     DispMovies
+  },
+  methods: {
+    
   }
 }
 </script>
