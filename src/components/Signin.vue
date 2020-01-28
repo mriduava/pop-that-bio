7baca6aeebf062bf73bf2b0fc8ff0d60457fcb3b
kais <kais2020@yahoo.com>
2020-01-28 11:01:50 +0100

    hej

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -3,14 +3,14 @@
  <h1></h1>
    
     <div class="row">
-    <form class="col s3 offset-s4 card-panel purple lighten-5 truncate">
+    <form class="col l3  s6 offset-l4 card-panel purple lighten-5 truncate">
         <div class="brand-logo">
              <h4 class="pink-text text-pink darken-4">POP THAT BIO</h4>
              <h2 class="pink-text text-pink darken-4">Sign In</h2>
    </div>
       <div class="row">
         
-        <div class="input-field col s6 ">
+        <div class="input-field col l6 s12 ">
           <input  id="User name" type="text" class="validate">
           <label for="User name">User Name</label>
         </div>
@@ -18,7 +18,7 @@
       </div>
     
       <div class="row">
-        <div class="input-field col s6">
+        <div class="input-field col l6 s12">
           <input id="password" type="password" class="validate">
           <label for="password">Password</label>
         </div>
@@ -27,8 +27,25 @@
          <button class="btn pink accent-1" @click.prevent="test () " >Logga in</button>
           
          </div>
-    </form>
+    </form> 
   </div>
+      <div class="row">
+        <div class="col s5 offset-s3">
+             <p class="pink-text text-pink darken-4">Medlemskapet för dig som älskar bio</p>
+             <div class="group3">
+             <div class="row">
+               <div class="col s4">Förtur till biljettsläpp</div>
+               <div class="col s4">Medlemskvällar</div>
+               <div class="col s4">Erbjudanden i kiosken</div>
+             </div>
+             </div>
+           <div class="butt">
+               <a href="test ()"><div class="card-panel  pink darken-1  text-align: center white-text text-darken-2">Bli medlem</div></a>
+          </div>
+     </div>         
+  </div>
+  
+
   </div>
 </template>
 
@@ -57,19 +74,32 @@ export default {
 </script>
 
 <style lang="css" scoped>
-.brand-logo{
+.brand-logo,p{
   font-size: 2.5rem;
   padding-left: 1.5%;
   font-family:Arial, Helvetica, sans-serif;
   text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.3);
  text-align: center;
-   text-shadow: 4px 6px 5px black;
+   text-shadow: 2px 2px 2px black;
 }
 h2{
    text-shadow: 4px 6px 5px black;
     text-align: center;
     font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
 }
-  
+.butt{
+  font-size: 1rem;
+  padding-left: 3%;
+  font-family:Arial, Helvetica, sans-serif;
+  text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.3);
+ text-align: center;
+   text-shadow: 2px 2px 2px black;
+
+}
+@media (max-width: 700px) {
+ h4 {
+    text-size-adjust:2%
+  }    
+}
 </style>
 
