efa727132cdc3c996e7e49b764e86ac16171ffec
Markus <mr.markus.coll@gmail.com>
2020-02-05 15:47:00 +0100

    fixed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
       <div class="nav-wrapper">
         <form @submit.prevent="search">
         <div class="input-field">
-          <input v-model="searchInput" id="search" type="search" required>
+          <input v-model="searchInput" class="autocomplete" id="search" type="search" required>
           <label for="search"></label>
           <label class="label-icon" for="search"><i class="material-icons">search</i></label>
           <i class="material-icons">close</i>
