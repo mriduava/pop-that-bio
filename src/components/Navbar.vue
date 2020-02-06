b104d275f38fe9efb586e064f572e2d658b13eca
Ola Wiebe <eeken07@gmail.com>
2020-02-06 12:11:38 +0100

    fix navbar with modal

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,64 +1,132 @@
 <template>
   <div class="container-fluid navbar navbar-fixed">
-    <nav  class="nav-extended">
-    <div class="nav-wrapper">
-        <router-link to="/"><a class="brand-logo "><p>POP THAT BIO</p></a></router-link>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="fas fa-align-justify"></i></a>
-      <ul class="right hide-on-med-and-down">
-        <li class="nav-item">
+    <nav class="nav-extended">
+      <div class="nav-wrapper">
+        <router-link to="/">
+          <a class="brand-logo">
+            <p>POP THAT BIO</p>
+          </a>
+        </router-link>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+          <i class="fas fa-align-justify"></i>
+        </a>
+        <ul class="right hide-on-med-and-down">
+          <li class="nav-item">
             <router-link to="/movies" class="nav-link">FILMER</router-link>
           </li>
 
-         <li class="nav-item">
+          <li class="nav-item">
             <router-link to="/about" class="nav-link">OM OSS</router-link>
           </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/signin">LOGGA IN</router-link>
+          <li class="nav-item">
+            <div class="nav-link modal-trigger" data-target="modal-login">LOGGA IN</div>
           </li>
-      
-      </ul>
+          <li class="nav-item">
+            <div class="nav-link modal-trigger" data-target="modal-signup">SKAPA KONTO</div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- SIGN UP MODAL -->
+    <div id="modal-signup" class="modal">
+      <div class="modal-content">
+        <h4>Skapa konto</h4>
+        <br />
+        <form id="signup-form">
+          <div class="input-field">
+            <input type="email" id="signup-email" required />
+            <label for="signup-email">Epost adress</label>
+          </div>
+          <div class="input-field">
+            <input type="password" id="signup-password" required />
+            <label for="signup-password">Välj lösenord</label>
+          </div>
+          <button class="btn yellow darken-2 z-depth-0">Skapa konto</button>
+        </form>
+      </div>
     </div>
-  </nav>
 
-  <ul class="sidenav" id="mobile-demo">
-     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <router-link to="/mypage"><i class="large material-icons   white-text text-grey lighten-5">account_circle</i></router-link>
-    <li class="nav-item">
-    <router-link to="/"><a class="brand-logo "><p class=" white-text text-grey lighten-5 ">Hame</p></a></router-link>
+    <!-- LOGIN MODAL -->
+    <div id="modal-login" class="modal">
+      <div class="modal-content">
+        <h4>Logga in</h4>
+        <br />
+        <form id="login-form">
+          <div class="input-field">
+            <input type="email" id="login-email" required />
+            <label for="login-email">Epost adress</label>
+          </div>
+          <div class="input-field">
+            <input type="password" id="login-password" required />
+            <label for="login-password">Ditt lösenord</label>
+          </div>
+          <button class="btn yellow darken-2 z-depth-0">Logga in</button>
+        </form>
+      </div>
+    </div>
+
+    <ul class="sidenav" id="mobile-demo">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      <router-link to="/mypage">
+        <i class="large material-icons white-text text-grey lighten-5">account_circle</i>
+      </router-link>
+      <li class="nav-item">
+        <router-link to="/">
+          <a class="brand-logo">
+            <p class="white-text text-grey lighten-5">HEM</p>
+          </a>
+        </router-link>
       </li>
-    <li class="nav-item">
-            <router-link to="/movies" class="nav-link"><p class=" white-text text-grey lighten-5 ">FILMER</p></router-link>
-          </li>
+      <li class="nav-item">
+        <router-link to="/movies" class="nav-link">
+          <p class="white-text text-grey lighten-5">FILMER</p>
+        </router-link>
+      </li>
 
-          <li class="nav-item">
-            <router-link to="/about" class="nav-link"><p class=" white-text text-grey lighten-5 ">OM OSS</p></router-link>
-          </li>
-           <li class="nav-item">
-            <router-link to="/about" class="nav-link"><p class=" white-text text-grey lighten-5 ">Medlemmar</p></router-link>
-          </li> 
-          <li class="nav-item">
-            <router-link to="/about" class="nav-link"><p class=" white-text text-grey lighten-5 ">Frågor och svar</p></router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/signin"><p class=" white-text text-grey lighten-5 ">Kundservice</p></router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/signin"><p class=" white-text text-grey lighten-5 ">LOGGA IN</p></router-link>
-          </li>
-  </ul>
- 
+      <li class="nav-item">
+        <router-link to="/about" class="nav-link">
+          <p class="white-text text-grey lighten-5">OM OSS</p>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/about" class="nav-link">
+          <p class="white-text text-grey lighten-5">Medlemmar</p>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/about" class="nav-link">
+          <p class="white-text text-grey lighten-5">Frågor och svar</p>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/signin">
+          <p class="white-text text-grey lighten-5">Kundservice</p>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/signin">
+          <p class="white-text text-grey lighten-5">LOGGA IN</p>
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
-// import M from 'materialize-css'
+import M from 'materialize-css'
 export default {
   mounted() {
-    
-    var elems = document.querySelectorAll('.carousel');
+    var elems = document.querySelectorAll(".carousel");
     this.$M.Carousel.init(elems);
-    setTimeout(this.$M.Carousel.init(elems), 1000)  
-  }, 
-}
+    setTimeout(this.$M.Carousel.init(elems), 1000);
+
+    var modals = document.querySelectorAll(".modal");
+    M.Modal.init(modals);
+
+    var items = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(items);
+  }
+};
 </script>
 
 <style lang="css" scoped>
@@ -70,13 +138,21 @@ nav {
   padding: 0 14.8%;
   border-bottom: 1px solid rgb(213, 187, 47);
   background: rgb(100, 10, 60);
-  background: -webkit-linear-gradient(to top, rgb(156, 36, 100), rgba(197, 49, 99, 0.5));
-  background: linear-gradient(to bottom, rgb(117, 9, 67), rgba(197, 49, 99, 0.5));
-   text-shadow: 2px 4px 1px rgb(12, 1, 1);
+  background: -webkit-linear-gradient(
+    to top,
+    rgb(156, 36, 100),
+    rgba(197, 49, 99, 0.5)
+  );
+  background: linear-gradient(
+    to bottom,
+    rgb(117, 9, 67),
+    rgba(197, 49, 99, 0.5)
+  );
+  text-shadow: 2px 4px 1px rgb(12, 1, 1);
   font-family: borntogrille;
-  }
+}
 
-.sidenav{
+.sidenav {
   background-color: rgba(107, 22, 72, 0.788);
 }
 
@@ -84,7 +160,7 @@ nav {
   font-size: 2.5rem;
   padding-left: 1.5%;
   font-family: borntogrille;
-   text-shadow: 1px 6px 1px rgb(12, 1, 1);
+  text-shadow: 1px 6px 1px rgb(12, 1, 1);
 }
 
 .our-brand-logo:hover {
@@ -108,7 +184,6 @@ nav {
   background: rgb(150, 38, 97);
 }
 
-
 @media (min-width: 481px) and (max-width: 767px) {
   nav {
     padding: 0;
@@ -126,5 +201,4 @@ nav {
     font-size: 1.9rem;
   }
 }
-
 </style>
