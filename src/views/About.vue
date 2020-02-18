ac6bb73c7b4bf043962f510e9ff8c3be47d32faa
Md Naim hossain arif <thk.naim@gmail.com>
2020-02-18 23:33:53 +0100

    added photo

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,12 +5,40 @@
         <h4 class="title-text center">A bout us...</h4>
         <hr class="hr-style" />
 
-        <div class="honey-comb"></div>
+        <div class="combs">
+          <div class="honey-comb">
+            <img src="https://www.athentech.com/wp-content/uploads/2014/04/portrait-1.jpg" alt />
+          </div>
+          <div class="honey-comb">
+            <img src="https://www.athentech.com/wp-content/uploads/2014/04/portrait-1.jpg" alt />
+          </div>
+          <div class="honey-comb">
+            <img src="https://www.athentech.com/wp-content/uploads/2014/04/portrait-1.jpg" alt />
+          </div>
+          <div class="honey-comb">
+            <img src="https://www.athentech.com/wp-content/uploads/2014/04/portrait-1.jpg" alt />
+          </div>
+        </div>
+
+        <div class="combs2">
+          <div class="honey-comb">
+            <img src="https://www.athentech.com/wp-content/uploads/2014/04/portrait-1.jpg" alt />
+          </div>
+          <div class="honey-comb">
+            <img src="https://www.athentech.com/wp-content/uploads/2014/04/portrait-1.jpg" alt />
+          </div>
+          <div class="honey-comb">
+            <img src="https://www.athentech.com/wp-content/uploads/2014/04/portrait-1.jpg" alt />
+          </div>
+        </div>
       </div>
 
       <div class="row">
         <div class="col s12 tack center">
-          <h6>Tack så mycket!</h6>
+          <h6>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et maxime accusantium facilis qui officiis quam harum nihil
+            voluptatem mollitia omnis laboriosam quasi at, doloribus culpa odit veritatis rem necessitatibus beatae?
+          </h6>
         </div>
       </div>
     </div>
@@ -25,7 +53,6 @@
         <div class="row about">
           <div class="col s12">
             <h4 class="center">Contacts...</h4>
-            <hr class="hr-style2" />
 
             <div class></div>
           </div>
@@ -78,21 +105,69 @@ export default {
 
 .title-text {
   font-family: queen;
-  font-size: 10rem;
+  font-size: 8.5rem;
   padding-top: 50px;
-  background: linear-gradient(to right, rgb(255, 128, 9) 30%, #1abe59 70%);
-  
+  background: linear-gradient(to right, rgb(237, 44, 255) 30%, #03e74f 70%);
+  -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
-
+.combs,
+.combs2 {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.combs2 {
+  position: relative;
+  top: -46px;
+}
 .honey-comb {
-  width: 200px;
+  max-width: 200px;
   height: 200px;
-  background: #1abe59;
-  clip-path: polygon(3% 0, 100% 0, 100% 100%, 0 100%);
+  margin: 0.4%;
+  border: 2px solid slategray;
+  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+}
+
+.honey-comb img {
+  width: 100%;
 }
 hr {
   position: relative;
   top: -50px;
+}
+
+@media (min-width: 1024px) and (max-width: 1280px) {
+  .combs2 {
+    top: 0;
+  }
+  .honey-comb {
+    max-width: 140px;
+    min-width: 100px;
+    height: 140px;
+  }
+}
+@media (min-width: 300px) and (max-width: 812px) {
+  .container-fluid {
+    position: relative;
+    top: -70px;
+    margin-bottom: -73px;
+  }
+  .title-text {
+    font-family: queen;
+    font-size: 5rem;
+  }
+
+  hr {
+    top: -30px;
+  }
+  .combs2 {
+    top: 0;
+  }
+  .honey-comb {
+    max-width: 120px;
+    min-width: 120px;
+    height: 120px;
+  }
 }
 </style>
